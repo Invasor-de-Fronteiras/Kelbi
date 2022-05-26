6ace6c3fbee5fd7364e0ab7a9209739cdb3d8b71
Yslan Ramos <yslanramos.r@gmail.com>
2022-05-25 21:44:51 -0300

    begin drag handler in icon

--- www/kelbi/src/components/LogoIcon.tsx
+++ www/kelbi/src/components/LogoIcon.tsx
@@ -2,5 +2,22 @@ import React from 'react';
 import LogoImg from '../assets/logo.png';
 
 export function LogoIcon() {
-  return <img src={LogoImg} width='303' height='114' />;
+  const onHold = (active: boolean) => {
+    // TODO: move to launcher.ts
+    //@ts-ignore
+    window.external.beginDrag(active);
+  };
+
+  return (
+    <img
+      src={LogoImg}
+      width='303'
+      height='114'
+      onMouseDown={() => onHold(true)}
+      onMouseUp={() => onHold(false)}
+      onMouseLeave={() => onHold(false)}
+      onTouchStart={() => onHold(true)}
+      onTouchEnd={() => onHold(false)}
+    />
+  );
 }
