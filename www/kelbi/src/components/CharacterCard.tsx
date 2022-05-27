4fe5bf39428f0020bea3269f2ec8a97d0bfd421c
Yslan Ramos <yslanramos.r@gmail.com>
2022-05-27 01:13:14 -0300

    move css from character-card to an own css file

--- www/kelbi/src/components/CharacterCard.tsx
+++ www/kelbi/src/components/CharacterCard.tsx
@@ -20,62 +20,25 @@ export function CharacterCard({ char, tabIndex, isSelected, onSelect }: Characte
       onClick={onSelect}
       style={{
         backgroundColor: isSelected ? '#f83' : '#fff',
-        marginBottom: '10px',
-        width: '200px',
-        padding: '10px',
       }}
     >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <span
-          style={{
-            textTransform: 'uppercase',
-            fontSize: '.9rem',
-          }}
-        >
-          {char.weapon}
-        </span>
+      <div className='flex flex-row justify-between items-center'>
+        <span className='char-weapon'>{char.weapon}</span>
       </div>
-      <h3
-        style={{
-          fontSize: '1.5rem',
-          fontWeight: 'bold',
-          textTransform: 'uppercase',
-        }}
-      >
-        {char.name}
-      </h3>
+      <h3 className='char-name'>{char.name}</h3>
       <div
         style={{
           marginTop: '5px',
           maxWidth: '200px',
         }}
       >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-          }}
-        >
-          <span style={{ flex: 1 }}>HR {char.HR}</span>
-          {char.GR ? <span style={{ flex: 1 }}>GR {char.GR}</span> : null}
+        <div className='flex flex-row justify-between items-center'>
+          <span className='flex-1'>HR {char.HR}</span>
+          {char.GR ? <span className='flex-1'>GR {char.GR}</span> : null}
         </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-          }}
-        >
-          <span style={{ flex: 1 }}>Last Login</span>
-          <span style={{ flex: 1 }}>{normalizeDate(char.lastLogin)}</span>
+        <div className='flex flex-row justify-between items-center'>
+          <span className='flex-1'>Last Login</span>
+          <span className='flex-1'>{normalizeDate(char.lastLogin)}</span>
         </div>
       </div>
     </div>
