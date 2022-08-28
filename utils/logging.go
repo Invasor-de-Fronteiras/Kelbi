770c44bbf90261dbc2c72553b2587817999fe794
Yslan Ramos <yslanramos.r@gmail.com>
2022-08-28 00:11:40 -0300

    fix: remove logging file (#21)
    the file tends to grow a lot and becomes unsustainable.

--- utils/logging.go
+++ utils/logging.go
@@ -8,15 +8,9 @@ import (
 )
 
 func NewLogger() (logger *zap.Logger, err error) {
-	prodEncoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
 	devEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
 
-	logFile, _ := os.OpenFile("logging.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-
-	writer := zapcore.AddSync(logFile)
-
 	core := zapcore.NewTee(
-		zapcore.NewCore(prodEncoder, writer, zap.DebugLevel),
 		zapcore.NewCore(devEncoder, zapcore.AddSync(os.Stdout), zap.DebugLevel),
 	)
 
