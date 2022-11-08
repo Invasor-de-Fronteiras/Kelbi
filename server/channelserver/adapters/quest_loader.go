22a1b2fb5ef673f9899166fe7246bf27dddaf6d5
Yslan Ramos <yslanramos.r@gmail.com>
2022-11-08 20:03:11 -0300

    feat: database quest struct

--- server/channelserver/adapters/quest_loader.go
+++ server/channelserver/adapters/quest_loader.go
@@ -1,8 +1,20 @@
 package adapters
 
+type QuestCategory = string
+
+const (
+	Diva QuestCategory = "DIVA"
+)
+
 type Quest struct {
-	Id            string
-	QuestListType string
+	Id            string // auto incriment
+	Name          string
+	Objective     string
+	Category      QuestCategory
+	Enabled       bool
+	Metadata      string //
+	QuestBinSize  int32
+	QuestListSize int32
 	QuestListBin  string
 	QuestBin      string
 }
