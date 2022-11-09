5b815b94314faa7934243a0e27fe8e857aa37b4e
Yslan Ramos <yslanramos.r@gmail.com>
2022-11-09 11:12:55 -0300

    feat: select all quest list by skip

--- server/channelserver/adapters/quest_loader_db.go
+++ server/channelserver/adapters/quest_loader_db.go
@@ -2,7 +2,6 @@ package adapters
 
 import (
 	"erupe-ce/common/byteframe"
-	"fmt"
 	"strconv"
 
 	"github.com/jmoiron/sqlx"
@@ -16,19 +15,24 @@ func NewQuestLoaderInDb(db *sqlx.DB) QuestLoader {
 	return &QuestLoaderInDb{db: db}
 }
 
+func (_ *QuestLoaderInDb) IdFromFilename(id string) (questId uint64, period string, season uint64, err error) {
+	questId, _ = strconv.ParseUint(id[0:5], 10, 32)
+	period = string(id[5])
+	season, _ = strconv.ParseUint(string(id[6]), 10, 32)
+	return
+}
+
 func (ql *QuestLoaderInDb) QuestBinById(id string) (questBin []byte, err error) {
-	questId, _ := strconv.ParseUint(id[0:5], 10, 32)
-	period := string(id[5])
-	season, _ := strconv.ParseUint(string(id[6]), 10, 32)
-	err = ql.db.QueryRow("SELECT quest_bin FROM quests WHERE quest_id = $1 AND period = $2 AND season = $3", questId, period, season).Scan(&questBin)
+	questId, period, season, _ := ql.IdFromFilename(id)
+	query := "SELECT quest_bin FROM quests WHERE quest_id = $1 AND period = $2 AND season = $3"
+	err = ql.db.QueryRow(query, questId, period, season).Scan(&questBin)
 	return
 }
 
 func (ql *QuestLoaderInDb) QuestListBinById(id string) (questListBin []byte, err error) {
-	questId, _ := strconv.ParseUint(id[0:5], 10, 32)
-	period := string(id[5])
-	season, _ := strconv.ParseUint(string(id[6]), 10, 32)
-	err = ql.db.QueryRow("SELECT quest_list_bin FROM quests WHERE quest_id = $1 AND period = $2 AND season = $3", questId, period, season).Scan(&questListBin)
+	questId, period, season, _ := ql.IdFromFilename(id)
+	query := "SELECT quest_list_bin FROM quests WHERE quest_id = $1 AND period = $2 AND season = $3"
+	err = ql.db.QueryRow(query, questId, period, season).Scan(&questListBin)
 	return
 }
 
@@ -40,7 +44,7 @@ func (ql *QuestLoaderInDb) QuestCount() (count uint16, err error) {
 func (ql *QuestLoaderInDb) Quests(take uint16, skip uint16) (questList []byte, err error) {
 	bf := byteframe.NewByteFrame()
 
-	buffer, returnedCount, err := ql.NextQuests(take, skip)
+	buffer, returnedCount, err := ql.QuestListBuffer(take, skip)
 
 	if err != nil {
 		return nil, err
@@ -67,48 +71,43 @@ func (ql *QuestLoaderInDb) Quests(take uint16, skip uint16) (questList []byte, e
 	return
 }
 
-type IteratorQuest struct {
-	QuestId       uint32 `db:"quest_id"`
-	Period        string `db:"period"`
-	Season        uint8  `db:"season"`
-	QuestListSize uint16 `db:"quest_list_bin_size"`
+type QuestListBin struct {
+	QuestList []byte `db:"quest_list_bin"`
 }
 
-func (ql *QuestLoaderInDb) NextQuest(skip uint16) (iter *IteratorQuest, err error) {
-	iter = &IteratorQuest{}
-	err = ql.db.Get(iter, "SELECT quest_id, period, season, quest_list_bin_size FROM quests WHERE enabled = true LIMIT 1 OFFSET $1", skip)
+func (ql *QuestLoaderInDb) QuestListBin(take uint16, skip uint16) (quests []QuestListBin, err error) {
+	quests = []QuestListBin{}
+	query := "SELECT quest_list_bin FROM quests WHERE enabled = true LIMIT $1 OFFSET $2"
+	err = ql.db.Select(quests, query, take, skip)
 	return
 }
 
-func (ql *QuestLoaderInDb) NextQuests(take uint16, skip uint16) (buffer *byteframe.ByteFrame, count uint16, err error) {
+func (ql *QuestLoaderInDb) QuestListBuffer(take uint16, skip uint16) (buffer *byteframe.ByteFrame, count uint16, err error) {
 	buffer = byteframe.NewByteFrame()
 	count = 0
 
-	var bufferSize uint16 = 0
-	var maxBufferSize uint16 = 64_000
+	var bufferSize = 0
+	var maxBufferSize = 64_000
 
 	for bufferSize <= maxBufferSize {
 
-		iter, err := ql.NextQuest(skip)
+		quests, err := ql.QuestListBin(take, skip+count)
 
 		if err != nil {
 			break
 		}
 
-		if (iter.QuestListSize + bufferSize) > maxBufferSize {
-			break
+		for _, quest := range quests {
+			size := len(quest.QuestList)
+
+			if (size + bufferSize) > maxBufferSize {
+				break
+			}
+
+			count++
+
+			buffer.WriteBytes(quest.QuestList)
 		}
-
-		skip++
-
-		questListBin, err := ql.QuestListBinById(fmt.Sprintf("%d%s%d", iter.QuestId, iter.Period, iter.Season))
-
-		if err != nil {
-			return nil, 0, err
-		}
-
-		buffer.WriteBytes(questListBin)
-		count++
 	}
 
 	return
