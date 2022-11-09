1e3667d16ab71afc44a3b0c5403aa8451dfc70f0
Yslan Ramos <yslanramos.r@gmail.com>
2022-11-08 22:48:30 -0300

    feat: `NextQuest` implemented

--- server/channelserver/adapters/quest_loader_db.go
+++ server/channelserver/adapters/quest_loader_db.go
@@ -60,12 +60,13 @@ func (ql *QuestLoaderInDb) Quests(take uint16, skip uint16) (questList []byte, e
 }
 
 type IteratorQuest struct {
-	Id            string
-	QuestListSize uint16
+	Id            string `db:"id"`
+	QuestListSize uint16 `db:"quest_list_size"`
 }
 
 func (ql *QuestLoaderInDb) NextQuest(skip uint16) (iter IteratorQuest, err error) {
-
+	iter = IteratorQuest{}
+	err = ql.db.Get(iter, "SELECT quest_list_bin FROM quests LIMIT 1 OFFSET $1", skip)
 	return
 }
 
